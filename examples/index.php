cf19af9c7ca7a451ee31656bd1d2c1d8d3becf33
Kevin Gilbert <kevin@kegisiteweb.com>
2016-02-11 22:21:59 -0500

    Updated example

--- examples/index.php
+++ examples/index.php
@@ -1,9 +1,28 @@
 <?php
 
+use KeGi\NetscapeCookieFileHandler\Configuration\Configuration;
+use KeGi\NetscapeCookieFileHandler\Exception\NetscapeCookieFileHandlerException;
 use KeGi\NetscapeCookieFileHandler\Handler;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 echo 'Example' . PHP_EOL . PHP_EOL;
 
-new Handler();
+try {
+
+    $configuration = (new Configuration())->setCookieDir(__DIR__ . '/cookies');
+    $cookieJar = (new Handler($configuration))->parseFile('example.txt');
+
+    var_dump(json_encode($cookieJar->getAll()));
+
+} catch (NetscapeCookieFileHandlerException $e) {
+
+    echo 'Handler Exception :' . PHP_EOL . PHP_EOL;
+    echo $e->getMessage();
+
+} catch (Exception $e) {
+
+    echo 'An exception occured :' . PHP_EOL . PHP_EOL;
+    echo $e->getMessage();
+}
+
