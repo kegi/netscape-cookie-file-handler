23671fa50d409fe65dc133d7649b0a77986196cb
Kevin Gilbert <kevin@kegisiteweb.com>
2016-02-11 22:19:50 -0500

    Added Exception base exception class

---
+++ src/NetscapeCookieFileHandler/Exception/NetscapeCookieFileHandlerException.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace KeGi\NetscapeCookieFileHandler\Exception;
+
+use Exception;
+
+class NetscapeCookieFileHandlerException extends Exception
+{
+    /**
+     * @param string         $message
+     * @param int            $code
+     * @param Exception|null $previous
+     */
+    public function __construct(
+        $message,
+        $code = 0,
+        Exception $previous = null
+    ) {
+
+        $message = 'NetscapeCookieFileHandlerException : ' . $message;
+
+        parent::__construct($message, $code, $previous);
+    }
+}
