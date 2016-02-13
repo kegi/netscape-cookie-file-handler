38651c1919f7049205f7408767b3a0551d6684c5
Kevin Gilbert <kevin@kegisiteweb.com>
2016-02-12 22:29:06 -0500

    Added unit tests for configurations

---
+++ tests/NetscapeCookieFileHandler/Configuration/ConfigurationTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace KeGi\NetscapeCookieFileHandler\Tests\Configuration;
+
+use KeGi\NetscapeCookieFileHandler\Configuration\Configuration;
+use KeGi\NetscapeCookieFileHandler\Configuration\ConfigurationInterface;
+use PHPUnit_Framework_TestCase;
+
+class ConfigurationTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testCookieDirParameterTest()
+    {
+
+        $dir = 'foo';
+
+        $configuration = new Configuration();
+        $configuration = $configuration->setCookieDir($dir);
+
+        $this->assertTrue(
+            ($configuration instanceof ConfigurationInterface),
+            'setCookieDir need to return ConfigurationInterface'
+        );
+
+        $this->assertEquals(
+            $dir,
+            $configuration->getCookieDir(),
+            'holy cow'
+        );
+    }
+}
