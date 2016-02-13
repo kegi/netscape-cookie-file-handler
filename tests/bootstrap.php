4d13c6a95fa35d12ded693fd1563512a9565c67d
Kevin Gilbert <kevin@kegisiteweb.com>
2016-02-12 22:28:40 -0500

    Added unit tests bootstrap

---
+++ tests/bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+/*default timezone*/
+
+date_default_timezone_set('UTC');
+
+/*composer autoloader*/
+
+$vendorPath = realpath(__DIR__ . '/../vendor');
+$autoloadFile = $vendorPath . DIRECTORY_SEPARATOR . 'autoload.php';
+
+if (!file_exists($autoloadFile)) {
+    throw new Exception('Unable to locate composer autoloader');
+}
+
+require_once $autoloadFile;
+
+
